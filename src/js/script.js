adb39a84c01a8b1a8ad6114560835175d145f5de
Rachel Mendes <rachelmendesnepomuceno@gmail.com>
2020-09-15 17:30:54 -0300

    teste: mostra news

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,46 @@
 
 window.onload = function(){
 
-var url = 'https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
+var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=jp&' +	  
           'apiKey=0e87d15ebad2463b803afd25220483a5';
-var req = new Request(url, {headers: new Headers ({"X-Requested-With":"news"})});
+var req = new Request(url);
 fetch(req)
 
 	.then(function(response){
-		response.json().then(function(data){
-			console.log(data); 
-			console.log('data-inside'); 
-			console.log(data.articles[0].title);
-			var d = new Date(data.articles[1].publishedAt);
-			document.getElementById("teste2").innerHTML = data.articles[1].title;
-			document.getElementById("teste2").href = data.articles[1].url;
-			document.getElementById("teste-img").src = data.articles[1].urlToImage;	
-			document.getElementById("teste-fonte").innerHTML = data.articles[1].source.name;
-			document.getElementById("teste-t").innerHTML = data.articles[1].description;
-			document.getElementById("teste-time").innerHTML = d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate();			
+		response.json().then(function(data){ 
 
-			for(i=0; i < data.articles[i].length; i++){
-				console.log(i);
+			console.log("Total de artigos: " + data.articles.length);			
+			var i;
+			var j = 1;
+			for (i = 0; i < 6; i++){
+				
+				var dt = new Date(data.articles[i].publishedAt);	
+				
 				console.log(data.articles[i].title);
-
+				console.log(data.articles[i].url);
+				console.log(data.articles[i].urlToImage);
+				console.log(data.articles[i].source.name);
+				console.log(data.articles[i].description);
+				console.log(dt.getFullYear() + "/" + dt.getMonth() + "/" + dt.getDate());
+							
+				document.getElementById("news-a" + j).innerHTML = data.articles[i].title;
+				document.getElementById("news-a" + j).href = data.articles[i].url;
+				document.getElementById("news-img-a" + j).href = data.articles[i].url;				
+				document.getElementById("news-img" + j).src = data.articles[i].urlToImage;	
+				document.getElementById("news-source" + j).innerHTML = data.articles[i].source.name;
+				document.getElementById("news-text" + j).innerHTML = data.articles[i].description;
+				document.getElementById("news-time" + j).innerHTML = dt.getFullYear() + "/" + dt.getMonth() + "/" + dt.getDate();					
+	
+				j++;
+				console.log("j é: " + j);
 			}
+			
 		});
 	})
 
 	.catch(function(err){ 
 		console.error('Failed retrieving information', err);
 	});
-	
-console.log("data"); 
 
 };
