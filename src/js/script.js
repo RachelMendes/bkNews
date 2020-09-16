a676ad1e6613f6a2235225ce1d2d96ac2dc163eb
Rachel Mendes <rachelmendesnepomuceno@gmail.com>
2020-09-15 21:11:51 -0300

    Mostrar News

--- src/js/script.js
+++ src/js/script.js
@@ -13,27 +13,59 @@ fetch(req)
 			console.log("Total de artigos: " + data.articles.length);			
 			var i;
 			var j = 1;
-			for (i = 0; i < 6; i++){
+			var newsContainer = $('#news-container');
+			console.log(newsContainer);
+			newsContainer.empty();
+			var newsRow;	
+			
+			for (i = 0; i < data.articles.length; i++){
 				
-				var dt = new Date(data.articles[i].publishedAt);	
+				var newsImg = $('<img class="img-responsive img-thumbnail" alt="Foto">');
+				var newsURL = $('<a target="_blank">');
+				var newsTitle = $('<h2 class="title-news">');
+				var newsSource = $('<h3 class="src-news">');
+				var newsDate = $('<h6 class="time-news">');
+				var newsDesc = $('<p align="justify">');
 				
-				console.log(data.articles[i].title);
-				console.log(data.articles[i].url);
-				console.log(data.articles[i].urlToImage);
-				console.log(data.articles[i].source.name);
-				console.log(data.articles[i].description);
-				console.log(dt.getFullYear() + "/" + dt.getMonth() + "/" + dt.getDate());
-							
-				document.getElementById("news-a" + j).innerHTML = data.articles[i].title;
-				document.getElementById("news-a" + j).href = data.articles[i].url;
-				document.getElementById("news-img-a" + j).href = data.articles[i].url;				
-				document.getElementById("news-img" + j).src = data.articles[i].urlToImage;	
-				document.getElementById("news-source" + j).innerHTML = data.articles[i].source.name;
-				document.getElementById("news-text" + j).innerHTML = data.articles[i].description;
-				document.getElementById("news-time" + j).innerHTML = dt.getFullYear() + "/" + dt.getMonth() + "/" + dt.getDate();					
+				var dt = new Date(data.articles[i].publishedAt);
+				
+				newsImg.attr('src', data.articles[i].urlToImage);
+				newsImg.attr('href', data.articles[i].url);				
+				
+				newsURL.append(data.articles[i].title);
+				newsURL.attr('href', data.articles[i].url);
+				newsTitle.append(newsURL);
+				
+				newsSource.append(data.articles[i].source.name);
+				newsDate.append(dt.getFullYear() + "/" + dt.getMonth() + "/" + dt.getDate());
+				newsDesc.append(data.articles[i].description);
+				
+				var infoNews = $('<div class="info-news">');
+				infoNews.append(newsTitle);
+				infoNews.append(newsSource);	
+				infoNews.append(newsDate);
+				infoNews.append(newsDesc);	
+
+				var newsImgURL = $('<a target="_blank">');
+				newsImgURL.append(newsImg);			
+				
+				var itemNews = $('<div class="item-news">');
+				itemNews.append(newsImgURL);
+				itemNews.append(infoNews);
+
+				var newsLine = $('<hr class="bd-line">');	
+				
+				var newsColumn = $('<div class="col-lg-4 col-sm-6">');
+				newsColumn.append(itemNews);
+				newsColumn.append(newsLine);	
 	
-				j++;
-				console.log("j é: " + j);
+				if(i%3 == 0){
+					newsRow = $('<div class="row">');					
+				};
+				
+				newsRow.append(newsColumn);
+				newsContainer.append(newsRow);
+				
 			}
 			
 		});
